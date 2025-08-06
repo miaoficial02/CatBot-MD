2b997f021a8c856204a604d54eb5c366ded79451
Its.mia.oficial <hibqbqxdd@gmail.com>
2025-08-06 01:16:42 -0300

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,18 +1,19 @@
 const handler = async (m, { conn, usedPrefix, command }) => {
   const texto = `
-🌐 𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝘿𝙚 𝙁𝙚𝙣𝙧𝙮𝙨𝘽𝙤𝙩 🚀
+🐈 𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝘿𝙚 𝗖𝗮𝘁-𝗕𝗼𝘁 🐈
 
 ✨ Únete a nuestra comunidad, comparte ideas, reporta errores, o simplemente charla con otros usuarios. ¡Eres bienvenido!
 
-📂 𝙇𝙞𝙨𝙩𝙖 𝘿𝙚 𝙂𝙧𝙪𝙥𝙤𝙨 𝘿𝙤𝙣𝙙𝙚 𝙀𝙨𝙩𝙖 𝙁𝙚𝙣𝙧𝙮𝙨 𝘽𝙤𝙩
-1️⃣  𝙂𝙧𝙪𝙥𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝘿𝙚 𝙁𝙚𝙣𝙧𝙮𝙨 (𝘾𝙚𝙧𝙤 𝙎𝙪𝙗𝘽𝙤𝙩)  
-https://chat.whatsapp.com/LTqtyYVc91oKdM4N0hZdko?mode=r_c
+📂 𝙇𝙞𝙨𝙩𝙖 𝘿𝙚 𝙂𝙧𝙪𝙥𝙤𝙨 𝘿𝙤𝙣𝙙𝙚 𝗖𝗮𝘁-𝗕𝗼𝘁
+
+1️⃣  𝙂𝙧𝙪𝙥𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝘿𝙚 𝗖𝗮𝘁-𝗕𝗼𝘁 (𝘾𝙚𝙧𝙤 𝙎𝙪𝙗𝘽𝙤𝙩)  
+𝑙𝑖𝑛𝑘: *𝗦𝗜𝗡 𝗚𝗥𝗨𝗣𝗢 𝗔𝗨𝗡*
 
 2️⃣  𝙂𝙧𝙪𝙥𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝙎𝙪𝙗𝘽𝙤𝙩  
-https://chat.whatsapp.com/FkBNSxVurdR2dX6m81qaZI?mode=r_c
+𝑙𝑖𝑛𝑘: *𝗦𝗜𝗡 𝗚𝗥𝗨𝗣𝗢 𝗔𝗨𝗡*
 
-3️⃣  𝙂𝙧𝙪𝙥𝙤 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 𝘾𝙤𝙡𝙤𝙗𝙤𝙧𝙖𝙘𝙞𝙤𝙣 𝙒𝙖𝙜𝙪𝙧𝙞 𝙄𝘼 𝙭 𝙁𝙚𝙣𝙧𝙮𝙨𝘽𝙤𝙩  
-https://chat.whatsapp.com/Fqks1pKKlRLH9UbelmeDck?mode=r_c
+3️⃣  𝗖𝗮𝗻𝗮𝗹 𝗢𝗳𝗶𝗰𝗶𝗮𝗹 𝗱𝗲 𝗖𝗮𝘁-𝗕𝗼𝘁 𝘆 𝘁𝗲𝘀𝘁. 
+𝑙𝑖𝑛𝑘: *𝗦𝗜𝗡 𝗖𝗔𝗡𝗔𝗟 𝗔𝗨𝗡*
 
 ⚠️ Respeta las normas de cada grupo.
 
@@ -24,10 +25,10 @@ https://chat.whatsapp.com/Fqks1pKKlRLH9UbelmeDck?mode=r_c
     text: texto.trim(),
     contextInfo: {
       externalAdReply: {
-        title: "𝑭𝒆𝒏𝒓𝒚𝒔𝑩𝒐𝒕-𝑴𝑫",
+        title: "𝐶𝑎𝑡-𝐵𝑜𝑡",
         body: "*Únete a nuestros grupos oficiales*",
         thumbnailUrl: 'https://files.catbox.moe/j6ci3o.jpg', // Puedes cambiar la imagen
-        sourceUrl: "https://github.com/El-brayan502/NyanCatBot-MD",
+        sourceUrl: "https://github.com/miaoficial02/0CatBot-MD",
         mediaType: 1,
         renderLargerThumbnail: true
       }
